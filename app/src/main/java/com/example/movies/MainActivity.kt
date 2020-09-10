186a2f989a035aafdcc5a384cb3aa4fe21057731
virginiadepaula <virginiapscampos@gmail.com>
2020-09-10 17:55:04 -0300

    Fragment

--- app/src/main/java/com/example/movies/MainActivity.kt
+++ app/src/main/java/com/example/movies/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.movies
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
@@ -10,22 +11,45 @@ import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
 
 
+
 class MainActivity : AppCompatActivity() {
     @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.elevation = 0f
 
         val titulosTab = resources.getStringArray(R.array.titulos_tab)
+
         val moviesAdapter = MoviesPageAdapter(this, titulosTab)
         viewPager.adapter = moviesAdapter
+
 
         TabLayoutMediator(tabLayout,viewPager){ tab, position ->
             tab.text = titulosTab[position]
         }.attach()
 
 
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+               //     viewPager.setCurrentItem(2)
+                }
+            }
 
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+    override fun onResume() {
+        super.onResume()
+ //       viewPager.currentItem = 2
     }
 
 }
